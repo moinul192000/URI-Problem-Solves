db4217be4a6ef38a3502820eaff2b32ab32fc3a8
Moinul Haq <moinul192000@gmail.com>
2018-02-16 14:51:20 +0600

    Create 1005.java

---
+++ 1005.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		float A, B, med;
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextFloat();
+        B = sc.nextFloat();
+        med = (float) (((A * 3.5) + (B * 7.5)) / (3.5 + 7.5));
+        String media = String.format("MEDIA = %,.5f", med);
+        System.out.print(media +"\n");
+	}
+
+}
