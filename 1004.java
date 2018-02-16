a85d803059ed5708544012de45938efe3f01cff3
Moinul Haq <moinul192000@gmail.com>
2018-02-16 14:43:11 +0600

    Create 1004.java

---
+++ 1004.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) { 
+        int n1, n2,PROD;
+        Scanner sc = new Scanner(System.in);
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        PROD = n1 * n2;
+        System.out.println("PROD = "+PROD);
+    }
+}
